83736505ed23921271cdd02d562264e11782b61a
AlexShyba <AlexShyba@51f1ba6a-a474-4e4d-b802-3a3369e95a95>
2011-11-16 15:43:22 +0000

    added new dynamic fields

---
+++ Branches/v2/Sitecore.SharedSource.SearchCrawler.DynamicFields/Content/ContentDepthField.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.SharedSource.Searcher.Utilities;
+
+namespace Sitecore.SharedSource.SearchCrawler.DynamicFields.Content
+{
+    public class ContentDepthField : BaseDynamicField
+    {
+        public override string ResolveValue(Item item)
+        {
+            Assert.ArgumentNotNull(item, "item");
+            return SearchHelper.FormatNumber(item.Axes.Level);
+        }
+    }
+}
---
+++ Branches/v2/Sitecore.SharedSource.SearchCrawler.DynamicFields/Content/DefaultIconField.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+
+namespace Sitecore.SharedSource.SearchCrawler.DynamicFields.Content
+{
+    public class DefaultIconField : BaseDynamicField
+    {
+        public override string ResolveValue(Item item)
+        {
+            Assert.ArgumentNotNull(item, "item");
+            return item.Appearance.Icon.ToLowerInvariant().Contains("document.png") ? "1" : null;
+        }
+    }
+}
